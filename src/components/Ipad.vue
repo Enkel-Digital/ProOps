525492fd4456bc4e058b5dc658dc59873de5a29e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-23 22:06:43 +0800

    Created Ipad component and added to Ipad view: 1.  Created scaffolded Ipad component. 2.  Added scaffolded Ipad component to the Ipad view.

---
+++ src/components/Ipad.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button>Button representing a iPad</button>
+    <p>{{ id }}</p>
+    <p v-if="available">Available</p>
+    <p v-else>In Use</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ipad",
+  data: function() {
+    return {
+      available: true,
+      id: 1
+    };
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/ipad.vue
+++ src/views/ipad.vue
@@ -1,13 +1,17 @@
 <template>
-    <p>scaffold</p>
+  <div>
+    <ipad />
+  </div>
 </template>
 
 <script>
+import ipad from "@/components/Ipad.vue";
+
 export default {
-    
-}
+  components: {
+    ipad
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
