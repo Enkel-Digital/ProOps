77990007fa2b13a195c56aa91e740473659eb5ea
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-28 00:54:14 +0800

    Added firebase package and auth setup code: 1.  Added firebase as a dependency. 2.  Added setup code in main.js to setup the use of firebase and also     added a auth state change event listener to only initialize the app     after firebase auth has been setup.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-Vue.config.productionTip = false
+import firebase from "firebase";
+import autofocus from "./directives/autofocus";
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false;
+
+// Register global custom directive called `v-autofocus`
+Vue.directive("autofocus", autofocus);
+
+// firebaseConfig auto generated in project settings
+firebase.initializeApp({
+  apiKey: "AIzaSyCuJg1Qkmhg2LnYUzvRKJLQrHKiYXkEVnA",
+  authDomain: "proops-mobile.firebaseapp.com",
+  databaseURL: "https://proops-mobile.firebaseio.com",
+  projectId: "proops-mobile",
+  storageBucket: "proops-mobile.appspot.com",
+  messagingSenderId: "939804252839",
+  appId: "1:939804252839:web:cee2a99765052347a75982"
+});
+
+// Wait for firebase to finish initialization before creating the app.
+// So that the router navigation wont break due to invalid auth
+firebase.auth().onAuthStateChanged(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount("#app");
+});
