864e5666bb2cba6bd975c7cfbd37902184bf550d
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-28 01:08:01 +0800

    Updated the routes module: 1.  File has been reformatted with eslint. 2.  Home view has is now mounted on /home instead of / 3.  Mounted the new welcome component on / 4.  Mounted login view on /login

--- src/router/index.js
+++ src/router/index.js
@@ -1,55 +1,60 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 /**
  * The route uses lazy loading for the components
- * 
+ *
  * route level code-splitting
  * this generates a separate chunk (about.[hash].js) for this route
  * which is lazy-loaded when the route is visited.
-*/
+ */
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('@/views/Home.vue')
+    path: "/",
+    name: "welcome",
+    component: () => import("@/views/Welcome.vue")
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/About.vue')
+    path: "/home",
+    name: "home",
+    component: () => import("@/views/Home.vue")
   },
   {
-    path: '/ipad',
-    name: 'ipad',
-    component: () => import('@/views/ipad.vue')
+    path: "/about",
+    name: "about",
+    component: () => import("@/views/About.vue")
   },
   {
-    path: '/walkie',
-    name: 'walkie',
-    component: () => import('@/views/Walkie.vue')
+    path: "/ipad",
+    name: "ipad",
+    component: () => import("@/views/ipad.vue")
   },
   {
-    path: '/team',
-    name: 'team',
-    component: () => import('@/views/Team.vue')
+    path: "/walkie",
+    name: "walkie",
+    component: () => import("@/views/Walkie.vue")
   },
   {
-    path: '/faulty',
-    name: 'faulty',
-    component: () => import('@/views/Faulty.vue')
+    path: "/team",
+    name: "team",
+    component: () => import("@/views/Team.vue")
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/Login.vue')
+    path: "/faulty",
+    name: "faulty",
+    component: () => import("@/views/Faulty.vue")
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue")
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
