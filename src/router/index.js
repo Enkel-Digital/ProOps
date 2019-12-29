c7ea4a21ec61b38c66c407de08d8f41fd3748b4e
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-12-29 16:34:13 +0800

    Implemented router gaurd function: 1.  Created a new module to export an AuthType enum object. 2.  Implemented router beforeEach method router gaurd function to check     auth by looking at the auth status of the current user compared to     the one specified by the routes use AuthType enum. 3.  Updated all the route to add meta data, specifying their individual     auth requirements.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import firebase from "firebase";
+
+// Import AuthType Enum
+import AuthType from "./AuthType";
 
 Vue.use(VueRouter);
 
@@ -14,47 +18,91 @@ const routes = [
   {
     path: "/",
     name: "welcome",
-    component: () => import("@/views/Welcome.vue")
+    component: () => import("@/views/Welcome.vue"),
+    meta: { Auth_requirements: AuthType.public_only }
   },
   {
     path: "/home",
     name: "home",
-    component: () => import("@/views/Home.vue")
+    component: () => import("@/views/Home.vue"),
+    meta: { Auth_requirements: AuthType.private }
   },
   {
     path: "/about",
     name: "about",
-    component: () => import("@/views/About.vue")
+    component: () => import("@/views/About.vue"),
+    meta: { Auth_requirements: AuthType.public }
   },
   {
     path: "/ipad",
     name: "ipad",
-    component: () => import("@/views/ipad.vue")
+    component: () => import("@/views/ipad.vue"),
+    meta: { Auth_requirements: AuthType.private }
   },
   {
     path: "/walkie",
     name: "walkie",
-    component: () => import("@/views/Walkie.vue")
+    component: () => import("@/views/Walkie.vue"),
+    meta: { Auth_requirements: AuthType.private }
   },
   {
     path: "/team",
     name: "team",
-    component: () => import("@/views/Team.vue")
+    component: () => import("@/views/Team.vue"),
+    meta: { Auth_requirements: AuthType.private }
   },
   {
     path: "/faulty",
     name: "faulty",
-    component: () => import("@/views/Faulty.vue")
+    component: () => import("@/views/Faulty.vue"),
+    meta: { Auth_requirements: AuthType.private }
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("@/views/Login.vue")
+    component: () => import("@/views/Login.vue"),
+    meta: { Auth_requirements: AuthType.public_only }
   }
 ];
 
 const router = new VueRouter({
   routes
 });
+
+// Function that returns an object with bool values of auth status.
+function auth(route) {
+  // Get auth requirements from first route object that matches with route navigated to
+  const { Auth_requirements } = route.matched[0].meta;
+
+  return {
+    public: Auth_requirements === AuthType.public,
+    public_only: Auth_requirements === AuthType.public_only,
+    private: Auth_requirements === AuthType.private
+  };
+}
+
+// Checks if user's current auth status matches required auth status for the route being accessed
+function AuthChecker(to, from, next) {
+  // Get current user from firebase
+  const currentUser = firebase.auth().currentUser;
+
+  // Get AuthStatus required for accessing the route.
+  const AuthType_required_is = auth(to);
+
+  /**
+   * @notice Call the next middleware provided by vue router with a route to go to.
+   * @notice Hard coded routes based on authentication status or proceed to route user requested for.
+   */
+  // If route is auth protected and user not logged in, redirect to welcome page
+  if (AuthType_required_is.private && !currentUser) next({ name: "welcome" });
+  // If route is public only and user is logged in, redirect to default private route of home
+  else if (AuthType_required_is.public_only && currentUser)
+    next({ name: "home" });
+  // Else, just continue navigation as per user request.
+  else next();
+}
+
+// Attach AuthChecker Middleware to run when navigation is made but before actual navigation.
+router.beforeEach(AuthChecker);
 
 export default router;
