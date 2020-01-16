eb10db42125b94e0b9f48079034cf827510fed36
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-01-16 22:36:29 +0800

    Updated users' mock data and started work on Team UI: 1.  Added email address into the user's mock data. 2.  Started working on the table element for the Team screen.

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
     <BackBtn />
-    <p>scaffold</p>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Team</th>
+        <th>Walkie</th>
+        <th>Ipad</th>
+      </tr>
+      <tr v-for="user in users" v-bind:key="user.email">
+        <td>{{ user.name }}</td>
+        <td>{{ user.team }}</td>
+        <!-- <td>{{ walkie.id }}</td>
+        <td>{{ ipad.id }}</td>-->
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 import BackBtn from "@/components/BackBtn.vue";
 
+// Tmp direct import of the users mock data to build the UI first.
+import users from "../store/mock_data/users";
+
 export default {
   components: {
     BackBtn
+  },
+  /* Update data to be real time binded so it updates as someone else changes it. */
+  data: function() {
+    return {
+      users
+    };
   }
 };
 </script>
