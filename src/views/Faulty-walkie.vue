677cc931c5767e3c96e1882e606c160ed3dfbbfe
Xin Jie <xin4545@hotmail.com>
2020-02-01 12:21:50 +0800

    Created Faulty-walkie view from Faulty-ipad

---
+++ src/views/Faulty-walkie.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <BackBtn />
+
+    <input
+      v-autofocus
+      type="number"
+      v-model="id"
+      placeholder="ID of faulty Walkie"
+      @keypress.enter="reportDevice"
+      required
+    />
+    <br />
+    <input
+      type="text"
+      v-model="description"
+      placeholder="Write your problem here"
+      @keypress.enter="reportDevice"
+      required
+    />
+
+    <p class="error">{{ error_msg }}</p>
+    <button @click="reportDevice">Report Device</button>
+  </div>
+</template>
+
+<script>
+import BackBtn from "@/components/BackBtn.vue";
+
+export default {
+  components: {
+    BackBtn
+  },
+  data() {
+    return {
+      id: "",
+      description: "",
+      error_msg: ""
+    };
+  },
+  methods: {
+    reportDevice() {
+      const current_user = {
+        email: this.$store.state.email,
+        name: this.$store.state.name
+      };
+
+      // Save user's email to vuex and update the user's name
+      this.$store.commit("new_problem", {
+        device_id: this.id,
+        description: this.description,
+        ...current_user,
+        device: "walkie"
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
