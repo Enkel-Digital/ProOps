728754579cbf53ea6d41ff1f679f69cf43803d4d
Jaime Loeuf <jaimeloeuf@gmail.com>
2020-02-07 22:15:03 +0800

    Added user check to Ipad component:     Added check to only allow same user to check in a device that is     currently checked out.

--- src/components/Ipad.vue
+++ src/components/Ipad.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="ipad-component">
-    <img alt="Ipad ICON" src="../assets/ipad.png" width="84" height="84" @click="clicked(ipad.id)" />
+    <img
+      alt="Ipad ICON"
+      src="../assets/ipad.png"
+      width="84"
+      height="84"
+      @click="clicked(ipad.id)"
+    />
 
     <p>{{ ipad.id }}</p>
     <p v-if="ipad.available">Available</p>
@@ -14,9 +20,21 @@ export default {
   props: ["ipad"],
   methods: {
     clicked(id) {
+      const selected_ipad = this.$store.state.ipads[id];
+
+      // If the device is checked out
+      if (!selected_ipad.available) {
+        // we want to ensure only the same user can check back in the device
+        if (selected_ipad.user !== this.$store.state.email) {
+          alert(`You did not checkout ipad "${id}" thus cannot check it in.`);
+          // Ends the function since current user is not allowed to check in device
+          return;
+        }
+      }
+
       alert(`You clicked ipad: ${id}`);
-      // Toggle the ipad's status in ipad object of vuex store
       this.$store.commit("toggle_ipad_status", id);
+
       // Route user back to the home page.
       this.$router.push({ name: "home" });
     }
