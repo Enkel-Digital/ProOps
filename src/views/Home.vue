95f73fae3b72f8a09df6dbf30d6e3b8ab845ddf6
Yi Peng <yipengyeoh@gmail.com>
2020-02-07 19:33:32 +0800

    Updated home screen CSS 1. Added teamshift and faulty icon 2. Created borders and change background color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,14 +9,31 @@
       />
       <br />
       Walkie Talkie
+      <br />
+        <br />
     </router-link>
     <router-link class="button" to="/ipad">
       <img alt="Ipad ICON" src="../assets/ipad.png" width="84" height="84" />
       <br />
       Ipad
+        <br />
+          <br />
     </router-link>
-    <router-link class="button" to="/team">Team Shift List</router-link>
-    <router-link class="button" to="/faulty">Faulty Devices</router-link>
+    <router-link class="button" to="/team">
+    <img 
+    alt= "Team Shift ICON"
+    src="../assets/teamshift.jpg" width= "84" height= "84" />
+<br/> 
+Team Shift List
+  <br />
+    <br />
+    </router-link>
+    <router-link class="button" to="/faulty">
+    <img alt= "Faulty Icon" 
+    src="../assets/faulty.jpg" width="84" height= "84" />
+    <br/>
+    Faulty Devices
+    </router-link>
     <button @click="logout">Logout</button>
   </div>
 </template>
@@ -45,19 +62,29 @@ export default {
   display: flex;
   flex-direction: column;
   text-align: center;
+  background-color: rgb(236, 133, 133)
+  
+}
+
+.button {
+  border-style: solid;
+  border-radius: 1em;
+  border-width: 0.2em;
+  background-color: rgb(153, 35, 35);
+  margin-bottom: 1.5em;
+  text-align: center;
+  text-decoration: none;
+  
 }
 
 button,
-.button,
 a:visited {
-  background-color: lightgrey;
-
-  max-width: 10em;
+  background-color: white;
+  max-width: 5em;
   margin-bottom: 1em;
-
   font-size: 3em;
-
   text-decoration: none;
   color: black;
+
 }
 </style>
